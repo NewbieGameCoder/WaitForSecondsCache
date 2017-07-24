6962cec260d6e149330daf3a23effd74c371d887
NewbieGameCoder <596809147@qq.com>
2017-07-24 12:02:07 +0800

    删掉错误的RealtimeCache

--- WaitForSecondsCache.cs
+++ WaitForSecondsCache.cs
@@ -33,34 +33,6 @@ public class WaitForSecondsCache
         return unit;
     }
 
-#if UNITY_5_4_OR_NEWER
-    static LinkedList<int> realTimeCacheQueue = new LinkedList<int>();
-    static Dictionary<int, WaitForSecondsRealtime> realTimeCache = new Dictionary<int, WaitForSecondsRealtime>(cacheSize);
-
-    public static WaitForSecondsRealtime WaitForRealTime(float waitingTime)
-    {
-        WaitForSecondsRealtime unit = null;
-        int clippedNumber = (int)(fractionalPartMultiplicand * waitingTime);
-
-        realTimeCache.TryGetValue(clippedNumber, out unit);
-        if (unit == null)
-        {
-            if (realTimeCache.Count >= cacheSize)
-            {
-                realTimeCache.Remove(realTimeCacheQueue.Last.Value);
-                realTimeCacheQueue.RemoveLast();
-            }
-            unit = new WaitForSecondsRealtime(waitingTime);
-            realTimeCache.Add(clippedNumber, unit);
-        }
-
-        if (realTimeCacheQueue.Count == 0) realTimeCacheQueue.AddFirst(clippedNumber);
-        else MoveToTop(clippedNumber, realTimeCacheQueue);
-
-        return unit;
-    }
-#endif
-
     static void MoveToTop(int elementValue, LinkedList<int> cacheQueue)
     {
         LinkedListNode<int> existElement = null;
